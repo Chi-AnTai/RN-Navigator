65a050c5fe37c42abd83e02ffa06a682413fe23b
Chi-AnTai <andy.chian.tai@gmail.com>
2020-11-03 18:21:53 +0800

    feat: test downgrade version

--- App.js
+++ App.js
@@ -33,7 +33,7 @@ const Screen2 = ({ navigator }) => {
 const Screen5 = ({ navigator }) => (
   <View style={[styles.screen, { backgroundColor: '#B9E3C6' }]}>
     <Button
-      title="Poppppp11"
+      title="Poppppp1123"
       onPress={() => navigator.pop()}
     /> 
   </View>
