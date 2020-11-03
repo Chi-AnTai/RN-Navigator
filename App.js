50cfe1efce46230e9aeeffcbaa9aed93fb697a8c
Chi-AnTai <andy.chian.tai@gmail.com>
2020-11-03 16:35:46 +0800

    feat: add feature B

--- App.js
+++ App.js
@@ -33,7 +33,7 @@ const Screen2 = ({ navigator }) => {
 const Screen5 = ({ navigator }) => (
   <View style={[styles.screen, { backgroundColor: '#B9E3C6' }]}>
     <Button
-      title="Pop"
+      title="Popp"
       onPress={() => navigator.pop()}
     />
   </View>
